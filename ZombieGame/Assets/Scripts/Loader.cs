124f273494749f8d4ba49abb132fb58474fafb10
fyreaken <55700749+fyreaken@users.noreply.github.com>
2019-11-30 23:46:31 +0000

    fixed Scene Changing, created Menu screens
    there was problems with unity's SceneManagement in the code, this has been fixed. GameManager.cs had to be changed to Loader.cs.
    
    Menu's have been made to so that you can lose the game, and return back to the game or main menu. No win method added yet.

---
+++ ZombieGame/Assets/Scripts/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Zombie Shooter Level 1");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
