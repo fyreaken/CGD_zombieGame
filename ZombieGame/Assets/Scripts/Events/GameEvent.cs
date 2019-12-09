334ce105d06177d79727d5320c9b75ffd574866b
fyreaken <55700749+fyreaken@users.noreply.github.com>
2019-12-09 17:11:43 +0000

    More MainMenu stuff

---
+++ ZombieGame/Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="GameEvent", menuName="Create GameEvent")]
+public class GameEvent : ScriptableObject
+{
+    List<GameEventListener> eventListeners = new List<GameEventListener>();
+
+	public void Raise()
+    {
+        for (int i = 0; i < eventListeners.Count; i++)
+            eventListeners[i].OnEventRaised();
+    }
+
+    public void AddListener(GameEventListener listener)
+    {
+        if (!eventListeners.Contains(listener))
+            eventListeners.Add(listener);
+    }
+
+    public void RemoveListener(GameEventListener listener)
+    {
+        if (eventListeners.Contains(listener))
+            eventListeners.Remove(listener);
+    }
+}
+
+
---
+++ ZombieGame/Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public UnityEvent Response;
+
+
+    private void OnEnable()
+    {
+        Event.AddListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.RemoveListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+}
